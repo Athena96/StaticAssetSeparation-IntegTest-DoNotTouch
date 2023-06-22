d78550a0b5186c50163f2f05cd73a5b5414c67c0
AWS Amplify <42283162+aws-aemilia@users.noreply.github.com>
2023-06-22 14:26:23 -0700

    add image component with remote url

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,18 @@ export default function Home() {
 
           ></Image>
         </figure>
+
+        <figure>
+          <figcaption>Remote url</figcaption>
+          <Image
+           alt="The guitarist in the concert."
+    src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
+    height={300}
+    width={900}
+    layout="responsive"
+          ></Image>
+        </figure>
+      
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
